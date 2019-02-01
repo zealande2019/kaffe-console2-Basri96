e058807930d0470f205d9729ad0982c0c64f8aed
Hasan Al-Basri <hasa0173@edu.easj.dk>
2019-02-01 14:05:24 +0100

    Kaffe opgave lavet

--- KaffeConsoleAppNetCore/Program.cs
+++ KaffeConsoleAppNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using kaffe;
 using System;
+using System.Collections.Generic;
 
 namespace KaffeConsoleAppNetCore
 {
@@ -8,10 +9,32 @@ namespace KaffeConsoleAppNetCore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Cortado cortado = new Cortado();
 
-            
+            Latte latte = new Latte();
+            FlatWhite flatWhite = new FlatWhite(12);
 
-           
+            List<Imælk> liste = new List<Imælk>();
+            liste.Add(cortado);
+            liste.Add(latte);
+
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine("Før cast " + item.MlMælk());
+
+                if (item is Latte)
+                {
+                    Latte L = new Latte();
+                    Console.WriteLine("Latte styrke " + L.Styrke());
+                }
+                else if (item is Cortado)
+                {
+                    Cortado c = item as Cortado;
+
+                }
+            }
+
 
         }
     }
